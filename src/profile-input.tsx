cdee2b140ad8c8c1362f431b619aba935b04f7cf
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-01-03 02:27:01 +0900

    feat: add terms and conditions

--- src/profile-input.tsx
+++ src/profile-input.tsx
@@ -1,15 +1,22 @@
+import Checkbox from "expo-checkbox";
 import React, { useState } from "react";
-import { TextInput, View } from "react-native";
+import { Dimensions, TextInput, View } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import TermsAndConditions from "./components/profile-input/terms-and-conditions";
 import Button from "./components/ui/button";
 import Text from "./components/ui/text";
 import FurColor from "./enum/progress.enum";
 import updateProfile from "./lib/progress/update-profile";
 
 export default function ProfileInputPage({ navigation }: any) {
+  const { width, height } = Dimensions.get("window");
+
   const [name, setName] = useState<string>("");
   const [catName, setCatName] = useState<string>("");
   const [furColor, setFurColor] = useState<FurColor>();
+  const [isChecked, setIsChecked] = useState(false);
+  const [isTermsOpen, setIsTermsOpen] = useState(false);
 
   console.log("FurColor", furColor);
 
@@ -20,56 +27,97 @@ export default function ProfileInputPage({ navigation }: any) {
     }
   };
   return (
-    <View className="flex h-screen w-screen flex-col items-stretch px-8 pb-16 pt-32">
-      <Text className="font-nunito-bold text-4xl">
+    <View className="relative flex h-screen w-screen flex-col items-stretch px-8 pb-8 pt-24">
+      <Text className="font-nunito-bold text-[36px]">
         Let's get your profile ready!
       </Text>
 
-      <Text className="mb-1 mt-6">Your Name</Text>
-      <TextInput
-        placeholder="Your Name"
-        editable
-        onChangeText={(value) => setName(value)}
-        className="h-[44px] w-full rounded-full border-[1px] border-gray-200 bg-white px-4 text-[16px]"
-      />
-      <Text className="mb-1 mt-6">Your Cat's Name</Text>
-      <TextInput
-        placeholder="Your Cat's Name"
-        editable
-        onChangeText={(value) => setCatName(value)}
-        className="text-md h-[44px] w-full rounded-full border-[1px] border-gray-200 bg-white px-4 text-[16px]"
-      />
-      <Text className="mb-1 mt-6">Fur Color</Text>
-      <RNPickerSelect
-        onValueChange={(value) =>
-          setFurColor(FurColor[value as keyof typeof FurColor])
-        }
-        items={Object.keys(FurColor).map((color, index) => ({
-          label: String(FurColor[color as keyof typeof FurColor]),
-          value: color,
-          key: index,
-        }))}
-        style={{
-          inputIOS: {
-            height: 44,
-            fontSize: 16,
-            paddingHorizontal: 16,
-            borderWidth: 1,
-            borderColor: "#E2E5E7",
-            borderRadius: 100,
-            color: "black",
-            backgroundColor: "white",
-            paddingRight: 30, // to ensure the text is always visible
-          },
-        }}
-        placeholder={{ label: "Select Fur Color", value: null }}
-      />
+      <View className="flex flex-1 flex-col">
+        <Text className="mb-1 mt-6">Your Name</Text>
+        <TextInput
+          placeholder="Your Name"
+          editable
+          onChangeText={(value) => setName(value)}
+          className="h-[44px] w-full rounded-full border-[1px] border-gray-200 bg-white px-4 text-[16px]"
+        />
+        <Text className="mb-1 mt-6">Your Cat's Name</Text>
+        <TextInput
+          placeholder="Your Cat's Name"
+          editable
+          onChangeText={(value) => setCatName(value)}
+          className="text-md h-[44px] w-full rounded-full border-[1px] border-gray-200 bg-white px-4 text-[16px]"
+        />
+        <Text className="mb-1 mt-6">Fur Color</Text>
+        <RNPickerSelect
+          onValueChange={(value) =>
+            setFurColor(FurColor[value as keyof typeof FurColor])
+          }
+          items={Object.keys(FurColor).map((color, index) => ({
+            label: String(FurColor[color as keyof typeof FurColor]),
+            value: color,
+            key: index,
+          }))}
+          style={{
+            inputIOS: {
+              height: 44,
+              fontSize: 16,
+              paddingHorizontal: 16,
+              borderWidth: 1,
+              borderColor: "#E2E5E7",
+              borderRadius: 100,
+              color: "black",
+              backgroundColor: "white",
+              paddingRight: 30, // to ensure the text is always visible
+            },
+          }}
+          placeholder={{ label: "Select Fur Color", value: null }}
+        />
+      </View>
+
+      {isTermsOpen && (
+        <>
+          <View
+            onTouchEnd={(e: any) => {
+              e.stopPropagation();
+              setIsTermsOpen(false);
+            }}
+            className="absolute z-50 z-50 bg-white"
+            style={{
+              right: width * 0.05 + 16,
+              top: height * 0.1 + 16,
+            }}
+          >
+            <Icon name="close-box" color="#4651D1" size={32} />
+          </View>
+          <TermsAndConditions />
+        </>
+      )}
+
+      <View className="mb-4 flex flex-row items-center justify-between gap-x-4">
+        <Checkbox
+          value={isChecked}
+          onValueChange={setIsChecked}
+          color="#4651D1"
+        />
+        <Text className="text-[18px]">
+          I agree to PurrTalk's{" "}
+          <Text
+            className="text-primary-900 underline"
+            onTouchEnd={(e: any) => {
+              e.stopPropagation();
+              setIsTermsOpen(true);
+            }}
+          >
+            Terms and Conditions{" "}
+          </Text>
+          and Privacy Policy.
+        </Text>
+      </View>
 
       <Button
-        title="Get started"
+        title="Continue"
         onPress={onPress}
-        className="mt-8"
-        disabled={!name || !catName || !furColor}
+        disabled={!name || !catName || !furColor || !isChecked}
       />
     </View>
   );
