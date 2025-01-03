ee27b249efa28a3a3d6197ddb44f24b33afb9727
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-01-04 00:42:08 +0900

    style: change the universal default text color

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -4,6 +4,9 @@ export default function Text(props: any) {
   const { ...rest } = props;
 
   return (
-    <ReactNativeText className={"font-nunito"} {...rest}></ReactNativeText>
+    <ReactNativeText
+      className={"font-nunito text-[#101720]"}
+      {...rest}
+    ></ReactNativeText>
   );
 }
