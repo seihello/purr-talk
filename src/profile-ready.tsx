95a946642e5e2197a1ca8072bf916335ea4d2a44
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-01-03 16:35:06 +0900

    style: complete the style of the profile ready page

--- src/profile-ready.tsx
+++ src/profile-ready.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import Button from "./components/ui/button";
 import Text from "./components/ui/text";
 import useProfile from "./hooks/use-profile";
@@ -10,8 +10,17 @@ export default function ProfileReadyPage({ navigation }: any) {
   if (!profile) return;
 
   return (
-    <View className="flex h-screen w-screen flex-col items-center px-8 pb-16 pt-32">
-      <Text className="font-nunito-bold text-[32px]">{`Nice to meet you,\n${profile.name} and ${profile.catName}!`}</Text>
+    <View className="flex h-screen w-screen flex-col items-center px-8 pb-8 pt-24">
+      <Image
+        source={require(`../assets/img/man_with_cat.png`)}
+        style={{
+          width: 348,
+          height: 417,
+        }}
+      />
+      <View className="flex flex-1 flex-col justify-center">
+        <Text className="font-nunito-bold text-[32px]">{`Nice to meet you,\n${profile.name} and ${profile.catName}!`}</Text>
+      </View>
       <Button
         title="Continue"
         onPress={() => {
