5b614441ad16cb6b7143e035d617a72ff5ad6321
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-01-04 14:34:47 +0900

    style: add wave to the init view | support lower screen

--- src/init.tsx
+++ src/init.tsx
@@ -6,21 +6,39 @@ import Text from "./components/ui/text";
 const PURR_TALK_TEXT_IMAGE_WIDTH = 216;
 
 export default function InitPage({ navigation }: any) {
-  const { width } = Dimensions.get("window");
+  const { width, height } = Dimensions.get("window");
 
   return (
-    <View className="flex h-screen w-screen flex-col items-center px-8 pb-8 pt-24">
-      <Image
-        source={require(`../assets/img/woman_with_cat.png`)}
-        style={{
-          width: 348,
-          height: 417,
-        }}
-      />
-      <View className="flex w-full flex-1 flex-col items-center justify-center py-16">
-        <Text className="mb-1 text-center font-nunito-bold text-[36px] leading-[41.66px] text-gray-900">
-          Welcome to
-        </Text>
+    <View className="flex h-screen w-screen flex-col items-center pb-8 pt-12">
+      <View className="flex shrink grow items-center justify-center">
+        <Image
+          source={require(`../assets/img/woman_with_cat.png`)}
+          style={{
+            width: width * 0.9,
+            height: (width * 0.9 * 417) / 348,
+          }}
+        />
+      </View>
+      <View className="flex w-full grow flex-col items-center justify-center">
+        <View className="relative w-full">
+          <View
+            className="absolute bottom-full"
+            style={{
+              transform: [{ translateY: 16 }],
+            }}
+          >
+            <Image
+              source={require(`../assets/img/pink_wave1.png`)}
+              style={{
+                width: width,
+                height: (width * 529) / 1572,
+              }}
+            />
+          </View>
+          <Text className="mb-1 text-center font-nunito-bold text-[36px] text-gray-900">
+            Welcome to
+          </Text>
+        </View>
         <Image
           source={require(`../assets/img/purr_talk_primary.png`)}
           style={{
@@ -32,12 +50,14 @@ export default function InitPage({ navigation }: any) {
           Find out what your cat’s really saying!
         </Text>
       </View>
-      <Button
-        title="Get started"
-        onPress={() => {
-          navigation.push("ProfileInput");
-        }}
-      />
+      <View className="flex w-full grow flex-col justify-end px-6">
+        <Button
+          title="Get started"
+          onPress={() => {
+            navigation.push("ProfileInput");
+          }}
+        />
+      </View>
     </View>
   );
 }
