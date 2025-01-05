53881eb52e80c49fb4d31765afc1648359e9c0b7
Seisuke Yamada <momoiropuchoman@gmail.com>
2025-01-05 14:25:54 +0900

    style: add the play button

--- src/translation.tsx
+++ src/translation.tsx
@@ -1,7 +1,7 @@
 import { Audio } from "expo-av";
 import { Sound } from "expo-av/build/Audio";
 import React, { useState } from "react";
-import { Image, ScrollView, View } from "react-native";
+import { Image, ScrollView, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Button from "./components/ui/button";
 import Text from "./components/ui/text";
@@ -55,7 +55,7 @@ export default function TranslationPage({ navigation, route }: any) {
           </Text>
         </ScrollView>
         <View className="my-6 h-[1px] w-full bg-gray-100" />
-        <View className="flex w-full flex-row justify-center">
+        <View className="flex w-full flex-row items-center justify-center gap-x-4">
           <Image
             source={require(`../assets/img/blue_pink_wave.png`)}
             style={{
@@ -63,13 +63,15 @@ export default function TranslationPage({ navigation, route }: any) {
               height: 43,
             }}
           />
-          <Button
-            title="P"
+          <TouchableOpacity
             onPress={() => {
               onPlayCatVoice();
             }}
-            icon={<Icon name="microphone" color="white" size={36} />}
-          />
+            className="flex h-[42px] w-[42px] flex-col items-center justify-center rounded-full bg-primary-900 px-[5px]"
+            activeOpacity={0.8}
+          >
+            <Icon name="play-outline" color="white" size={36} />
+          </TouchableOpacity>
         </View>
       </View>
       <View className="flex grow items-center justify-center">
